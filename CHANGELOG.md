d7db3b2f827bda406daf19a348d0e6a90a19463f
4uzhoy <4uzhoyy@gmail.com>
2025-06-19 15:38:10 +0500

    chore: update version to 1.0.2, add exports to barrel file, and clean up comments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.2
+
+- Add exports to barrel file
+
 ## 1.0.1
 
 - Format Readme.md
--- lib/tinygram.dart
+++ lib/tinygram.dart
@@ -1,6 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-// TODO: Export any libraries intended for clients of this package.
+export 'src/tinygram_bot.dart';
+export 'src/tinygram_chat.dart';
